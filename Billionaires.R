52bda793b09d0ea88447523fe9d3daa5a6d0fab3
RosebudAnwuri <rose_anwuri@yahoo.com>
2017-05-10 21:13:16 +0100

    Updated file with changes to detail crawler

--- Billionaires.R
+++ Billionaires.R
@@ -259,25 +259,98 @@ max_distance = function(row){
 year_list$longest_time_away = apply(year_list[,3:13],1,max_distance)
 write_csv(year_list,"year_list.csv")
 
-#After some work in Excel e.g. joining data columns togther to the original billionaire info
-#I'd like to get data on the other guys from the past 10 years (1929 people)
-year_list = read_csv("year_list.csv")
-parsing_list = year_list %>% filter(is.na(`Not Available`) ==T)
-other_billionaires = parsing_list$Name
-other_billionaires = as.character(other_billionaires)
-other_billionaires = str_trim(other_billionaires)
-other_billionaires=str_replace_all(other_billionaires,"[^0-9A-Za-z\\- ]",'')
-other_billionaires=str_replace_all(other_billionaires,"\\s+",' ')
-other_billionaires = str_replace_all(other_billionaires," ","-")
-other_billionaires=tolower(other_billionaires)
+# #After some work in Excel e.g. joining data columns togther to the original billionaire info
+# #I'd like to get data on the other guys from the past 10 years (1929 people)
+# year_list = read_csv("year_list.csv")
+# parsing_list = year_list %>% filter(is.na(`Not Available`) ==T)
+# other_billionaires = parsing_list$Name
+# other_billionaires = as.character(other_billionaires)
+# other_billionaires = str_trim(other_billionaires)
+# other_billionaires=str_replace_all(other_billionaires,"[^0-9A-Za-z\\- ]",'')
+# other_billionaires=str_replace_all(other_billionaires,"\\s+",' ')
+# other_billionaires = str_replace_all(other_billionaires," ","-")
+# other_billionaires=tolower(other_billionaires)
+# 
+# other_details = list()
+# for (i in 1672:length(other_billionaires)){
+#   dat=get_billionaire_info(other_billionaires[i])
+#   other_details = append(other_details,list(dat))
+#   print(paste0(round((i/length(other_billionaires))*100,2),"% done at ",i))
+# }
+# 
+# other_details = do.call("rbind",other_details)
+# other_details = cbind(other_billionaires,other_details)                                                                  
+# other_details = as.data.frame(other_details)
 
-other_details = list()
-for (i in 1672:length(other_billionaires)){
-  dat=get_billionaire_info(other_billionaires[i])
-  other_details = append(other_details,list(dat))
-  print(paste0(round((i/length(other_billionaires))*100,2),"% done at ",i))
+billionaire_data = read_csv("Billionaire Data.csv")
+
+billionaire_data %>% 
+  group_by(joining_year) %>% 
+  summarise(Number_of_comers = n()) %>%
+  filter(joining_year >2007) %>%
+  ggplot(aes(joining_year,Number_of_comers)) +
+  geom_line(aes(color="red")) 
+  
+  
+billionaire_data %>%
+  filter(joining_year >2007) %>%
+  filter(`Country of Citezenship`!="United States") %>%
+  group_by(joining_year,Sector) %>%
+  summarise(number_of_people = n()) %>%
+  top_n(1)
+  
+billionaire_data %>%
+  group_by(`Country of Citezenship`,Sector) %>%
+  summarise(number_of_people = n()) %>%
+  filter(number_of_people >1) %>%
+  top_n(1) %>%
+  arrange(-number_of_people)
+
+billionaire_data %>%
+  filter(no_of_comebacks >=2) %>%
+  nrow()
+url="http://stats.areppim.com/listes/list_billionairesx17xwor.htm"
+tbl = url %>% 
+  read_html %>%
+  html_node("body > table") %>%
+  html_table()
+tbl=setNames(tbl,c("Rank","Name","Worth","Age","Source of Wealth","Country of Citizenship"))
+lower_billionaires = tbl1[1061:nrow(tbl1),]
+
+names_to_URLs = function(name){
+  name = as.character(name)
+  name = str_trim(name)
+  name=str_replace_all(name,"[^0-9A-Za-z\\- ]",'')
+  name=str_replace_all(name,"\\s+",' ')
+  name = str_replace_all(name," ","-")
+  name=tolower(name)
+  return(name)
 }
 
-other_details = do.call("rbind",other_details)
-other_details = cbind(other_billionaires,other_details)                                                                  
-other_details = as.data.frame(other_details)
+get_billionaire_info = function(names_of_billionaires){
+  names_of_billionaires=names_to_URLs(names_of_billionaires)
+  url = paste0("https://www.forbes.com/profile/",names_of_billionaires,"/")
+  data=try(url %>% 
+             read_html %>% 
+             html_nodes(".stats li") %>% 
+             html_text(trim=T)
+           ,silent = T)
+  if(class(data) =="try-error"){
+    sector = NA
+    education = NA
+  }
+  sector = ifelse(length(data[grepl("Source of Wealth",data,T)])>0,data[grepl("Source of Wealth",data,T)],NA)
+  education = ifelse(length(data[grepl("Education",data,T)])>0,data[grepl("Education",data,T)],NA)
+  sector = str_replace_all(sector,".*\t|.*\n","")
+  education = str_replace_all(education,".*\t|.*\n","")
+  return(c(sector,education))
+}
+lower_billionaires$detail1=""
+lower_billionaires$detail2=""
+for (i in 3:nrow(lower_billionaires)){
+  dat=get_billionaire_info(lower_billionaires$Name[i])
+  #dat = list(dat)
+  lower_billionaires$detail1[i]=dat[1]
+  lower_billionaires$detail2[i]=dat[2]
+  print(paste0(round((i/nrow(lower_billionaires))*100,2),"% done at ",i))
+}
